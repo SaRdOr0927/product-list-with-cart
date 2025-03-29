557588e4053d212b7202d3f47a96980a92d3d668
Sardor <joraqulovasayyora1@gmail.com>
2025-03-29 16:36:49 +0500

    hover

--- main.js
+++ main.js
@@ -1,80 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const minusBtn = document.querySelector(".main__product-btn.minus");
-    const plusBtn = document.querySelector(".main__product-btn.plus");
-    const numberOutput = document.querySelector(".main__product-num");
-
-    let count = 0;
-
-    minusBtn.addEventListener("click", function () {
-        if (count > 0) {
-            count--;
-            numberOutput.textContent = count;
-        }
+const cartButton = [...document.querySelectorAll(".cart-button")];
+const mainProductNumbern = [...document.querySelectorAll(".main__product-number")];
+const elBtn = [...document.querySelectorAll('.main__product-btn')];
+const shopCartMenu = document.querySelector('.shop-cart__menu')
+const shopImg = document.querySelector('.shop-img')
+const cardimg = [...document.querySelectorAll('.card img')]
+console.log(cardimg)
+for (let l = 0; l < cartButton.length; l++) {
+    cartButton[l].addEventListener("click", function () {
+        cartButton[l].style.display = 'none';
+        mainProductNumbern[l].style.display = 'block';
+        shopImg.style.display = 'none';
+        shopCartMenu.style.display = 'block';
+        cardimg[l].style.border = '3px solid #C73B0F';
     });
-
-    plusBtn.addEventListener("click", function () {
-        count++;
-        numberOutput.textContent = count;
+}
+for (let i = 0; i < elBtn.length; i++) {
+    elBtn[i].addEventListener("click", function () {
+        prepare(this);  
     });
-});
+}
 
-const cartButton = document.querySelector(".cart-button")
-const mainProductNumbern = document.querySelector(".main__product-number")
+function prepare(btn) {
+    let parent = btn.closest('.main__product-number');
+    let number = parent.querySelector('.main__product-num');
+    let symbol = btn.getAttribute('data-symbol');
+    let count = parseInt(number.textContent);
 
-const b1 = document.querySelector("#b1");
-const b2 = document.querySelector("#b2");
-const b3 = document.querySelector("#b3");
-const b4 = document.querySelector("#b4");
-const b5 = document.querySelector("#b5");
-const b6 = document.querySelector("#b6");
-const b7 = document.querySelector("#b7");
-const b8 = document.querySelector("#b8");
-const b9 = document.querySelector("#b9");
+    if (symbol === "+") { 
+        count += 1;
+    } else if (symbol === "-" && count > 0) { 
+        count -= 1;  
+    }
 
-const n1 = document.querySelector("#n1");
-const n2 = document.querySelector("#n2");
-const n3 = document.querySelector("#n3");
-const n4 = document.querySelector("#n4");
-const n5 = document.querySelector("#n5");
-const n6 = document.querySelector("#n6");
-const n7 = document.querySelector("#n7");
-const n8 = document.querySelector("#n8");
-const n9 = document.querySelector("#n9");
-
-console.log(mainProductNumbern)
-b1.addEventListener("click", function () {
-    b1.style.display = 'none'
-    n1.style.display = 'block'
-})
-b2.addEventListener("click", function () {
-    b2.style.display = 'none'
-    n2.style.display = 'block'
-})
-b3.addEventListener("click", function () {
-    b3.style.display = 'none'
-    n3.style.display = 'block'
-})
-b4.addEventListener("click", function () {
-    b4.style.display = 'none'
-    n4.style.display = 'block'
-})
-b5.addEventListener("click", function () {
-    b5.style.display = 'none'
-    n5.style.display = 'block'
-})
-b6.addEventListener("click", function () {
-    b6.style.display = 'none'
-    n6.style.display = 'block'
-})
-b7.addEventListener("click", function () {
-    b7.style.display = 'none'
-    n7.style.display = 'block'
-})
-b8.addEventListener("click", function () {
-    b8.style.display = 'none'
-    n8.style.display = 'block'
-})
-b9.addEventListener("click", function () {
-    b9.style.display = 'none'
-    n9.style.display = 'block'
-})
+    number.textContent = count;
+}
